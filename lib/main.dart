0f41c5a2e9e0250e5a26a5086e432fc675d6561c
KevinBenaya <darkfaust063@gmail.com>
2022-03-03 17:49:12 +0700

    Perubahan Layout dan Set Rumus

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -38,12 +40,44 @@ class MyApp extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Padding(
-                      padding: EdgeInsets.symmetric(vertical: 210),
-                      child: Text('Suhu dalam Kelvin'),
+                      padding: EdgeInsets.only(top: 150),
+                      child: Text(
+                        'Suhu dalam Kelvin',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100,
                     ),
                     Padding(
-                      padding: EdgeInsets.only(left: 50),
-                      child: Text('Suhu dalam Reamur'),
+                      padding: EdgeInsets.only(top: 150),
+                      child: Text(
+                        'Suhu dalam Reamur',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 100),
+                      child: Text(
+                        '0',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 100),
+                      child: Text(
+                        '0',
+                        style: TextStyle(fontSize: 30),
+                      ),
                     ),
                   ],
                 ),
@@ -77,7 +111,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   double _inputUser = 0;
   double _kelvin = 0;
-  double reamur = 0;
+  double _reamur = 0;
+
+  kelvin() {
+    setState(() {
+      _inputUser = double.parse(retrive1.text);
+      _kelvin = _inputUser + 273;
+    });
+  }
+
+  reamur() {
+    setState(() {
+      _inputUser = double.parse(retrive1.text);
+      _reamur = 4 ~/ 5 * _inputUser;
+    });
+  }
+
+  TextEditingController retrive1 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -85,12 +135,80 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(''),
       ),
-      body: Center(
+      body: Container(
+        margin: EdgeInsets.all(8),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextFormField(
+              controller: retrive1,
+              decoration: InputDecoration(
+                hintText: 'Masukkan Suhu Dalam Celcius',
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 150),
+                    child: Text(
+                      'Suhu dalam Kelvin',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 150),
+                    child: Text(
+                      'Suhu dalam Reamur',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 100),
+                    child: Text(
+                      ' : $_kelvin',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 100),
+                    child: Text(
+                      ' : $_reamur',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 500,
+              padding: EdgeInsets.only(top: 16),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Konversi Suhu",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
